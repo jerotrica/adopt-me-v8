ee7f00fe679ea4a84b02a3a449d4b8dcb9fedf24
Jeronimo <jerotrica@gmail.com>
2023-01-17 21:23:18 -0300

    Carousel Module finished

---
+++ src/Components/fetchSearch.jsx
@@ -0,0 +1,15 @@
+async function fetchSearch({ queryKey }) {
+  const { animal, location, breed } = queryKey[1];
+
+  const res = await fetch(
+    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+  );
+
+  if (!res.ok) {
+    throw new Error(`Pet search not okay`);
+  }
+
+  return res.json();
+}
+
+export default fetchSearch;
